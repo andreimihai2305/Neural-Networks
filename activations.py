38565b4f644e221d5c7e8f71156e4e6952e4a2c8
Andrei Mihai <andreimihai2305is@gmail.com>
2023-05-23 16:49:31 +0300

    added tanh activation function

--- activations.py
+++ activations.py
@@ -1,13 +1,16 @@
 import math
 
 def sigmoid(nr):
-	return round(1 / (1 + math.exp(-nr)), 8)
+	return 1 / (1 + math.exp(-nr))
 
 
 def relu(nr):
 	return max(0.0, nr)
 
-
+def tanh(nr):
+	return (math.exp(2*nr) - 1) / (math.exp(2*nr) + 1)
 
 def no_activation(nr):
 	return nr
+
+	
